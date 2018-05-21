0a7375c2560acb00376d2158a8e3e8ab20df2894
Cristian Colorado <ccoloradoc@gmail.com>
2018-05-21 12:56:09 -0500

    Fix: Modifying lookup timeframe

--- services/cron/scripts/dm.js
+++ services/cron/scripts/dm.js
@@ -4,13 +4,14 @@ const TwitMessenger = require('commons').TwitMessenger;
 
 let watcherService = new WatcherService(config.mongo.uri, config.mongo.options);
 let now = Date.now();
-let delay = 15 * 60000;
+let delay = 5 * 60000;
 
 function inspect(user) {
   console.log(`>> User ${user.screen_name}`);
   watcherService.findLeadAndCount({
     owner: user.screen_name,
-    activated_on: {$gt: new Date(now - (2 * delay)), $lt : new Date(now - delay) }
+    // Activated in the last 20 min except latest 5min 
+    activated_on: {$gt: new Date(now - (4 * delay)), $lt : new Date(now - delay) }
   }, 50, 0, {}).then(result => {
     console.log(`>> We need to send ${result.leads.length} dms`)
     // Create Stream
