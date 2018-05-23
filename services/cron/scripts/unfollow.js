0d260f1502c17d77bd7609e17a88b74e2165ac6b
Cristian Colorado <cristian.colorado@hbr.org>
2018-05-23 16:09:57 -0500

    Cron: Fix cron range and order

--- services/cron/scripts/unfollow.js
+++ services/cron/scripts/unfollow.js
@@ -7,7 +7,7 @@ const Lead = require('commons').Lead;
 function unfollow(user, lead) {
   return new Promise((resolve, reject) => {
     let unfollower = new Unfollower(user.credentials);
-    // Unfollow  
+    // Unfollow
     unfollower.unfollow(lead.screen_name)
     .then(() => {
       logger.debug(`Unfollow ${lead.screen_name} [${lead.followers_count}]`);
@@ -17,7 +17,7 @@ function unfollow(user, lead) {
         if(err) reject(err);
         logger.debug(`Updated ${lead.screen_name}`);
         resolve(result);
-      });  
+      });
     })
     .catch(err => reject(err));
   });
@@ -27,14 +27,14 @@ function processUser(user) {
   return new Promise((resolve, reject) => {
     logger.debug(`Processing user ${user.screen_name}`);
     // Find a lead who has been adquired, not cleared nor activated
-    watcherService.findLeads({ 
+    watcherService.findLeads({
         owner: user.screen_name,
-        adquired_on: {$exists: true}, 
-        cleared_on: {$exists: false}, 
+        adquired_on: {$exists: true},
+        cleared_on: {$exists: false},
         activated_on: {$exists: false}
-        }, 
-        1, 0, 
-        { adquired_on: 1, followers_count: 1 }
+        },
+        1, 0,
+        { followers_count: 1, adquired_on: 1 }
       ).then(leads => {
         logger.debug(`Found ${leads.length} leads`)
         if(leads.length > 0) {
@@ -56,9 +56,9 @@ logger.debug('Running DM cronjob');
 watcherService.findUsers({}, 100).then(users => {
   logger.debug(`Found ${users.length} users`);
   let promises = users.map(user => processUser(user) );
-  
+
   Promise.all(promises)
   .then(watcherService.close)
   .catch(watcherService.close);
-  
+
 ;});
