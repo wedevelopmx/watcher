965e6ec3a05770fb8dcdbb5ee5e4065a57ce1a32
Cristian Colorado <cristian.colorado@hbr.org>
2018-06-06 15:29:26 -0500

    Home redirects to dashboard

--- restore.sh
+++ restore.sh
@@ -1,1 +1,1 @@
-sudo docker exec -it watcher_mongo_1 mongorestore --username monitor --password "m0n1t0r" --archive=data/db/monitor.gz --gzip
+sudo docker exec -it watcher_mongo_1 mongorestore --username monitor --password "m0n1t0r" --archive=/data/db/backup/monitor.gz --gzip
